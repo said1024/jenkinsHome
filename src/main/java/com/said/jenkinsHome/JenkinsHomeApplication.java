a6c207a7c74ff41af72cb6306f2104f7c30bda4b
said1024 <said1024.b@gmail.com>
2023-09-30 13:13:29 +0100

    jenkins 1

---
+++ src/main/java/com/said/jenkinsHome/JenkinsHomeApplication.java
@@ -0,0 +1,14 @@
+package com.said.jenkinsHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsHomeApplication.class, args);
+		System.out.println("test jenkins n° 1");
+	}
+
+}
---
+++ src/test/java/com/said/jenkinsHome/JenkinsHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.said.jenkinsHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
